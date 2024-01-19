1c062081b94ba0bf1add13b7a6785f2785c803ad
Shubham Kumar <hello@shubhkumar.in>
2024-01-19 14:20:13 +0530

    removed force new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -183,7 +183,7 @@ export default function Page() {
           </div>
         </Section>
 
-        <Section className="print-force-new-page scroll-mb-16">
+        <Section className="scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
@@ -199,7 +199,7 @@ export default function Page() {
             })}
           </div>
         </Section>
-        <Section className="print-force-new-page scroll-mb-16">
+        <Section className="scroll-mb-16">
           <h2 className="text-xl font-bold">Certifications</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.certifications.map((project) => {
