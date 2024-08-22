2aa9b0927f44251e940cfc6910e068c4a5366d54
Shubham Kumar <hello@shubhkumar.in>
2024-08-22 19:37:16 +0530

    print no avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,21 +50,24 @@ export default async function Page() {
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4  md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-4 print:grid print:grid-cols-2 print:gap-2">
         <div className="flex items-center justify-between col-start-1 col-span-2">
-          <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold text-[#1d4dd6] ">{about.name}</h1>
-            <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground print:text-[12px] col-start-1">
-              {about.about}
-            </p>
-            <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
-              <a
-                className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                href={about.locationLink}
-                target="_blank"
-              >
-                <GlobeIcon className="size-3" />
-                {about.location}
-              </a>
-            </p>
+          <div className="flex-1 space-y-1.5 print:flex">
+            <div className="print:flex-1 print:col-span-2">
+              <h1 className="text-2xl font-bold text-[#1d4dd6] ">{about.name}</h1>
+              <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground print:text-[12px] col-start-1">
+                {about.about}
+              </p>
+
+              <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
+                <a
+                  className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
+                  href={about.locationLink}
+                  target="_blank"
+                >
+                  <GlobeIcon className="size-3" />
+                  {about.location}
+                </a>
+              </p>
+            </div>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
               {RESUME_DATA.contact.email ? (
                 <Button
@@ -123,8 +126,8 @@ export default async function Page() {
             </div>
           </div>
 
-          <Avatar className="size-28">
-            <AvatarImage alt={about.name} src={about.avatarUrl} />
+          <Avatar className="size-28 print:hidden">
+            <AvatarImage alt={about.name} src={about.avatarUrl} className="print:hidden" />
             <AvatarFallback>{about.initials}</AvatarFallback>
           </Avatar>
         </div>
