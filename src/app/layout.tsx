b29a9a29ea6fa5eccb44fa3c9b61bb2ad594719a
Shubham Kumar <hello@shubhkumar.in>
2026-03-27 14:40:22 +0530

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,63 @@ import React from "react";
 
 
 
-export const metadata: Metadata = {
-  title: "CV",
-  description: "Resume",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  let data;
+  const BASE_URL = "https://api.shubhkumar.in";
+  try {
+    const res = await fetch(
+      `${BASE_URL}/api/publicmetadata`,
+      {
+        next: { revalidate: 86400 }, // 1 day
+      }
+    );
+
+    data = await res.json();
+  } catch {
+    data = {
+      title: "Shubham Kumar",
+      description: "Associate Lead Engineer",
+    };
+  }
+
+  return {
+    title: data.title,
+    description: data.description,
+    keywords: data.keywords,
+    authors: [{ name: data.author, url: data.url }],
+    creator: data.author,
+
+    icons: {
+      apple: "/apple-touch-icon.png",
+      icon: "/favicon.ico",
+    },
+
+    openGraph: {
+      type: "website",
+      locale: "en_US",
+      url: data.url,
+      title: data.title,
+      description: data.description,
+      siteName: data.siteName,
+      images: data.images,
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: data.title,
+      description: data.description,
+      creator: data.twitter,
+    },
+
+    robots: {
+      index: true,
+      follow: true,
+    },
+  };
+}
+
+
+
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
